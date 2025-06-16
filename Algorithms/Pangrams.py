d012c6c28533da603dff48c51158897f0224b95e
Nishant Bhandigare <nishantbhandigare@gmail.com>
2025-06-16 21:13:25 +0530

    general commit

---
+++ Algorithms/Pangrams.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    
+    s = s.lower()
+    letters = []
+    for letter in s:
+        if letter != " " and letter not in letters:
+            letters.append(letter)
+            
+    if len(letters) != 26:
+        return "not pangram"
+    else:
+        return "pangram"
+                
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
